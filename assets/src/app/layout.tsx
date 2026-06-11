23da93a4c2b3fb4242f42f77725648d3c125c6f0
Matheus Menezes <matheusmenezes278@gmail.com>
2026-06-11 00:44:43 -0400

    adiciona projeto devcard

--- assets/src/app/layout.tsx
+++ assets/src/app/layout.tsx
@@ -1,31 +1,11 @@
-import { Tabs } from "expo-router";
+import { Stack } from 'expo-router';
 
-export { Tabs} from "expo-router";
-
-export default function Rootlayout(){
-
-return (
-<Tabs initialRouteName="index">
-<Tabs.Screen name="index"/>
-<Tabs.Screen name="cadastro"/>
-<Tabs.Screen name="preview"/>         
-<Tabs.Screen name="sucesso"/>         
-       
-</Tabs>
-
-
-)
-
-
-
-}
-
-
-
-
-
-
-
-
-
-
+export default function Layout() {
+    return (
+        <Stack
+            screenOptions={{
+                headerShown: false,
+            }}
+        />
+    );
+} 
